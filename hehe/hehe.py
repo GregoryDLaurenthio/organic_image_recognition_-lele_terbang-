f914e4d7543bd37d39aeabfbb981fdebdf85f900
Venturio <venturiotalenta1703@gmail.com>
2024-11-25 22:19:05 -0500

    Another Update with wkwkwkkw

--- hehe/hehe.py
+++ hehe/hehe.py
@@ -1,2 +1,3 @@
 print ("I love Ventu Entu Ntu Tu U .")
 print ("I love you")
+print ("Aiyaiya Im a little butterfly")
--- hehe/hoho.py
+++ hehe/hoho.py
@@ -1,3 +1,3 @@
 print("Drey ey ey ey eya eya eeee aaaaa")
-
+print("Hello wolord")
 print ("changed hohohohoho")
